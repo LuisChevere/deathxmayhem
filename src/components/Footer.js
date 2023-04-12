3e91a5bea7b6cd511232dbce553ca4534de5a43f
Luis <hairchevere@gmail.com>
2023-04-12 10:57:25 -0500

    created footer with contact nav

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    
+    return (
+        <Footer classname=''>
+            <a href="/contact">Contact</a>
+
+        </Footer>
+    )
+
+}
+
+export default Footer;
