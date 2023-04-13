91021410aea02caad5cfff7fdbf0f11dec89897c
Luis <hairchevere@gmail.com>
2023-04-12 21:14:02 -0500

    updated Navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
-import DxM from '../assets/images/DeathxMayhemLogo.png';
+import Nav from 'react-bootstrap/Nav';
+import DxM from '../assets/images/DxM.png';
 
 
 function Navbar() {
 
     return (
-        <Navbar classname=''>
+        <Nav classname=''>
             <a href='/merch'>Merch</a>
             <a href='/crowley'>Crowley</a>
             <img className='dxm' alt='DeathxMayhemLogo' src={DxM} />
             <a href='/lilith'>Lilith</a>
             <a href='/calendar'>Show Dates</a>
-        </Navbar>
+        </Nav>
     )
 }
 
